4c2dba7f9a1d76b9ab7a3decc26ded1af3f178a0
Ansh <ansh@localhost>
2026-09-25 17:06:04 +0800

    chore: versionCode 5 for the 1.0.0 release build (Android blocks downgrades from older test builds)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.example.miles.wear"
         minSdk = 30
         targetSdk = 35
-        versionCode = 1
+        versionCode = 5
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
